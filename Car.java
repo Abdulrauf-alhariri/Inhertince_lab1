3ecdcf593137909db56785f00bc8337c13a3c494
Abdullrauf Alhariri <aboodaalhariri123.aa9@gmail.com>
2025-01-28 14:46:13 +0100

    Finshing the first task

---
+++ Car.java
@@ -0,0 +1,50 @@
+import java.awt.*;
+
+public abstract class Car {
+    protected int nrDoors; // Number of doors on the car
+    protected double enginePower; // Engine power of the car
+    protected double currentSpeed; // The current speed of the car
+    protected Color color; // Color of the car
+    protected String modelName; // The car model name
+
+    public Car(int nrDoors, Color color, double enginePower, String modelName) {
+        this.nrDoors = nrDoors;
+        this.color = color;
+        this.enginePower = enginePower;
+        this.modelName = modelName;
+    }
+
+    public int getNrDoors(){
+        return nrDoors;
+    }
+    public double getEnginePower(){
+        return enginePower;
+    }
+
+    public double getCurrentSpeed(){
+        return currentSpeed;
+    }
+
+    public Color getColor(){
+        return color;
+    }
+
+    public void setColor(Color clr){
+        color = clr;
+    }
+
+    public void startEngine(){
+        currentSpeed = 0.1;
+    }
+
+    public void stopEngine(){
+        currentSpeed = 0;
+    }
+
+    abstract void incrementSpeed(double amount);
+
+    abstract double speedFactor();
+
+    abstract void decrementSpeed(double amount);
+
+}
