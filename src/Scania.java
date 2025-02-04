4ef18652661d1e8bbf0a8b7db2088c650695ea78
antonbulow <antonbul@chalmers.se>
2025-02-04 09:46:49 +0100

    Scania

---
+++ src/Scania.java
@@ -0,0 +1,68 @@
+import java.awt.*;
+
+public class Scania extends Car {
+    private double angle;
+    public Scania() {
+        super(2,Color.black,300, "Scania");
+        this.angle = 0;
+    }
+
+    public void flatBedRaise(double amount){
+        double newAngle = this.angle + amount;
+
+        if(newAngle > 70) {
+            this.angle = 70;
+        } else {
+            this.angle = newAngle;
+        }
+
+    }
+
+    public void flatBedLower(double amount){
+        double newAngel = this.angle + amount;
+
+        if (newAngel < 0){
+            this.angle = 0;
+        } else {
+            this.angle = newAngel;
+        }
+    }
+
+    public double getFlatBedAngle() {
+        // Angle between 0 and 70
+        return this.angle;
+    }
+
+    @Override
+    public void move(){
+        Direction direction = getDirection();
+        double currentSpeed = getCurrentSpeed();
+        // Car move only if angle is 0
+        if (this.angle == 0) {
+            if (direction == Direction.NORTH) {
+                this.xAndY.y += currentSpeed;
+            } else if (direction == Direction.EAST) {
+                this.xAndY.x += currentSpeed;
+            } else if (direction == Direction.SOUTH) {
+                this.xAndY.y -= currentSpeed;
+            } else if (direction == Direction.WEST) {
+                this.xAndY.x -= currentSpeed;
+            }
+        }
+    }
+
+    @Override
+    void incrementSpeed(double amount) {
+
+    }
+
+    @Override
+    double speedFactor() {
+        return 0;
+    }
+
+    @Override
+    void decrementSpeed(double amount) {
+
+    }
+}
