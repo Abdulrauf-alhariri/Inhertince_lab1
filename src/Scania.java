40409e1d9e1bbba6e8611e6d61622e47db22eec9
LoveHellgren <lovehe@chalmers.se>
2025-02-07 00:20:03 +0100

    Fixat med "workshop"

--- src/Scania.java
+++ src/Scania.java
@@ -1,3 +1,4 @@
+
 import java.awt.*;
 
 public class Scania extends Truck {
---
+++ src/Workshop.java
@@ -0,0 +1,45 @@
+
+import java.util.*;
+import static java.awt.*;
+
+public class Workshop <T extends MotorVehicle> {
+
+    private int maximumVehicles; // Maximum vehicles that can be accepted simultaneously
+    private Set<Class<? extends T>> servicedVehicles; // Types of vehicles accepted for repair
+    protected List<T> vehiclesInRepair; // A list of the vehicles currently in the workshop
+
+    @SafeVarargs // Allows for any number of Vehicle classes (incl their subclasses) to be added as allowed
+    public Workshop(int maximumVehicles, Class<? extends T>...allowedVehicleTypes) {
+        this.maximumVehicles = maximumVehicles;
+        this.servicedVehicles = new HashSet<>(Arrays.asList(allowedVehicleTypes));
+        this.vehiclesInRepair = new ArrayList<>();
+    }
+    // If function called by the vehicle; returning a boolean allows it to know if admitted to workshop or not
+    public boolean vehicleEntry(T vehicle){
+        if ( ! servicedVehicles.contains(vehicle.getClass())){
+            return false;}
+        if (vehiclesInRepair.size() >= maximumVehicles){
+            return false;}
+        vehiclesInRepair.add(vehicle);
+        return true;}
+
+    public void vehicleExit(T vehicle){
+        vehiclesInRepair.remove(vehicle);    // do we need to know that it is the same instance of the class??
+    }
+
+
+    }
+/* TEST
+class Main {
+    public static void main(String[] args) {
+        Workshop<Car> carWorkshop = new Workshop<>(10, Volvo240.class, Saab95.class);{
+        Volvo240 aVolvo = new Volvo240();
+        Saab95 aSaab95 = new Saab95();
+        Truck aTruck = new Truck(2,black,100,"BIG TRUCK");
+
+        carWorkshop.vehicleEntry(aVolvo);
+        carWorkshop.vehicleEntry(aTruck);
+        }*/
+    }}
+
+
