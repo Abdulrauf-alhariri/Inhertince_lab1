aca0bad3db332c677aec1ec6a155be8a914c2905
LoveHellgren <lovehe@chalmers.se>
2025-02-10 23:27:29 +0100

    Updated "workshop" and tests

--- src/Workshop.java
+++ src/Workshop.java
@@ -3,11 +3,16 @@ import java.awt.*;
 import java.util.*;
 import java.util.List;
 
+/*Creates a workshop for MotorVehicles. In instantiation CAN define which Class and its subclasses
+* will be allowed to be serviced in general. within < >,  But also as arguments specify which Classes(Vehicles) of those that will be served,
+* these can be updated later. At the moment Vehicles can be serviced in several workshops at the same time, which could potentially cause problems. */
+
 public class Workshop <T extends MotorVehicle> {
 
     private int maximumVehicles; // Maximum vehicles that can be accepted simultaneously
     private Set<Class<? extends T>> servicedVehicles; // Types of vehicles accepted for repair
-    protected List<T> vehiclesInRepair; // A list of the vehicles currently in the workshop
+    protected List<T> vehiclesInRepair; // A list of the vehicles currently in the workshop. List gives option of indexing i.e. first-in, first-out
+    private static final Set<MotorVehicle> globalAllVehiclesInRepair = new HashSet<>(); //Keeps track which vehicles are already in service.
 
     @SafeVarargs // Allows for any number of Vehicle classes (incl their subclasses) to be added as allowed
     public Workshop(int maximumVehicles, Class<? extends T>...allowedVehicleTypes) {
@@ -16,18 +21,25 @@ public class Workshop <T extends MotorVehicle> {
         this.vehiclesInRepair = new ArrayList<>();
     }
     // If function called by the vehicle; returning a boolean allows it to know if admitted to workshop or not
-    public boolean vehicleEntry(T vehicle){
+    protected boolean vehicleEntry(T vehicle){
+        if (globalAllVehiclesInRepair.contains(vehicle)) {  // Won't add vehicle if already in repair
+            return false;}
         if ( ! servicedVehicles.contains(vehicle.getClass())){
             return false;}
         if (vehiclesInRepair.size() >= maximumVehicles){
             return false;}
+
+        globalAllVehiclesInRepair.add(vehicle);
         vehiclesInRepair.add(vehicle);
         return true;}
 
-    public void vehicleExit(T vehicle){
-        vehiclesInRepair.remove(vehicle);    // Will remove the same reference object of vehicle.
+    protected void vehicleExit(T vehicle){
+        vehiclesInRepair.remove(vehicle);// Will remove the same reference object of vehicle.
+        globalAllVehiclesInRepair.remove(vehicle);
     }
-
+    protected void addServicedVehicle(Class<? extends T> vehicleClass){
+        servicedVehicles.add(vehicleClass);
+    }
 
     }
 /* TEST */
@@ -45,6 +57,6 @@ class Main {
     }}
 */
 
-/* TEST */
+// TEST
 
 
--- test/WorkshopTest.java
+++ test/WorkshopTest.java
@@ -1,33 +1,37 @@
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.awt.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class WorkshopTest {
-    private Workshop<Car> saab95OnlyCarWorkshop;
+    private Workshop<Saab95> saab95OnlyCarWorkshop;
     private Workshop<Car> carWorkshop;
     private Workshop<Truck> truckWorkshop;
+    private Workshop<CarTransporter> carTransporterWorkshop;
+    private Workshop generalVehicleWorkshop;
 
     private Volvo240 volvoA;
     private Volvo240 volvoB;
     private Saab95 saab95A;
     private Truck truckA;
     private Truck truckB;
+    private CarTransporter carTransporterA;
 
     @BeforeEach
     void setUp() {
-        saab95OnlyCarWorkshop = new Workshop<>(10, Saab95.class);
-        carWorkshop = new Workshop<>(2, Car.class);
+        saab95OnlyCarWorkshop = new Workshop<Saab95>(10, Saab95.class);
+        carWorkshop = new Workshop<Car>(2, Saab95.class, Volvo240.class);
+        carTransporterWorkshop = new Workshop<>(10, CarTransporter.class);
         truckWorkshop = new Workshop<>(10, Truck.class);
+        generalVehicleWorkshop = new Workshop<>(3, Saab95.class, Volvo240.class, CarTransporter.class, Truck.class, Car.class);
 
         volvoA = new Volvo240();
         volvoB = new Volvo240();
         saab95A = new Saab95();
         truckA = new Truck(2, Color.black, 100, "BIG TRUCK");
         truckB = new Truck(2, Color.red, 80, "Small TRUCK");
+        carTransporterA = new CarTransporter();
     }
 
     @AfterEach
@@ -36,10 +40,34 @@ class WorkshopTest {
 
     @Test
     void vehicleEntry() {
-        saab95OnlyCarWorkshop.vehicleEntry(saab95A);
+
+        Assertions.assertTrue(saab95OnlyCarWorkshop.vehicleEntry(saab95A));
+        Assertions.assertFalse(saab95OnlyCarWorkshop.vehicleEntry(saab95A)); // Duplicate entry
+       // Assertions.assertFalse(saab95OnlyCarWorkshop.vehicleEntry(volvoA));  // Will give compile time error!
+
+
+        Assertions.assertFalse(truckWorkshop.vehicleEntry(carTransporterA)); // Truck workshop is not initiated with carTransporter.
+
+        Assertions.assertTrue(generalVehicleWorkshop.vehicleEntry(truckA));
+        Assertions.assertTrue(generalVehicleWorkshop.vehicleEntry(volvoA));
+        Assertions.assertTrue(generalVehicleWorkshop.vehicleEntry(carTransporterA));
+        Assertions.assertFalse(generalVehicleWorkshop.vehicleEntry(truckB)); // Should already be full
+        generalVehicleWorkshop.vehicleExit(truckA);
+        Assertions.assertTrue(generalVehicleWorkshop.vehicleEntry(truckB));
+
+
     }
 
     @Test
     void vehicleExit() {
+        carWorkshop.vehicleEntry(volvoA);
+        carWorkshop.vehicleEntry(saab95A);
+        assertTrue(carWorkshop.vehiclesInRepair.contains(volvoA));
+        assertTrue(carWorkshop.vehiclesInRepair.contains(saab95A));
+        carWorkshop.vehicleExit(volvoA);
+        assertFalse(carWorkshop.vehiclesInRepair.contains(volvoA));
+        carWorkshop.vehicleExit(saab95A);
+        assertFalse(carWorkshop.vehiclesInRepair.contains(saab95A));
+
     }
 }
